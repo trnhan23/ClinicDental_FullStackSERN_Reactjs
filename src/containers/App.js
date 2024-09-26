2b82eb9396e42d0db1b60126bafd3556e40e14ff
Trong Nhan <cnttute21110571@gmail.com>
2024-09-26 18:15:05 +0700

    design homepage

--- src/containers/App.js
+++ src/containers/App.js
@@ -14,13 +14,11 @@ import Home from '../routes/Home';
 // import Login from '../routes/Login';
 import Login from './Auth/Login';
 
-
 import Header from './Header/Header';
 import System from '../routes/System';
 
 import { CustomToastCloseButton } from '../components/CustomToast';
-import ConfirmModal from '../components/ConfirmModal';
-
+import HomePage from './HomePage/HomePage.js'
 class App extends Component {
 
     handlePersistorState = () => {
@@ -46,12 +44,12 @@ class App extends Component {
             <Fragment>
                 <Router history={history}>
                     <div className="main-container">
-                        <ConfirmModal />
                         {this.props.isLoggedIn && <Header />}
 
                         <span className="content-container">
                             <Switch>
                                 <Route path={path.HOME} exact component={(Home)} />
+                                <Route path={path.HOMEPAGE} component={(HomePage)} />
                                 <Route path={path.LOGIN} component={userIsNotAuthenticated(Login)} />
                                 <Route path={path.SYSTEM} component={userIsAuthenticated(System)} />
                             </Switch>
